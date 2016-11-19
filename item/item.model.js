6b42137eaad9b8198c658d3d5340fd521e623d2b
John Armstrong <j.armstrong484@gmail.com>
2016-11-20 12:50:19 +1300

    Created an item model

---
+++ item/item.model.js
@@ -0,0 +1,13 @@
+let mongoose = require('mongoose'),
+    Schema = mongoose.Schema;
+
+let itemSchema = new Schema({
+    name: String
+});
+
+let Item = mongoose.model('Item', itemSchema);
+
+module.exports = {
+    Model: Item,
+    Type: itemSchema
+};
