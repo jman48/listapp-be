5883ae6ac4caf7ab2d1208162e37ae738f16e5e7
John Armstrong <j.armstrong484@gmail.com>
2016-11-20 12:48:56 +1300

    Added an endpoint to delete a single list

--- list/list.controller.js
+++ list/list.controller.js
@@ -1,6 +1,6 @@
 let express = require('express'),
- listRouter = express.Router(),
- List = require('./list.model');
+    listRouter = express.Router(),
+    List = require('./list.model');
 
 /**
  * Get all lists
@@ -45,9 +45,30 @@ listRouter.post('/', (req, res) => {
 });
 
 /**
+ * Delete a single list by id
+ */
+listRouter.delete('/:id', (req, res) => {
+    List.findById(req.params.id, (err, list) => {
+        if (err) {
+            return handleError(err, res);
+        }
+
+        list.remove((err) => {
+            if (err) {
+                return handleError(err, res);
+            }
+
+            res.sendStatus(204);
+        });
+    });
+});
+
+/**
  * A generic error handler. Will log error and return status 500
  */
 function handleError(err, res) {
     console.log(`ERROR: ${err}`);
+
+    //Do not send error message as we do not want to expose any internal working or our application
     res.sendStatus(500);
 }
