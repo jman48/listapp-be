98f9313bf3b0c5ecdab43f897bca90fae8b92b7b
John Armstrong <j.armstrong484@gmail.com>
2016-11-20 12:39:04 +1300

    Added an endpoint to return a single list

--- list/list.controller.js
+++ list/list.controller.js
@@ -2,6 +2,23 @@ let express = require('express'),
  listRouter = express.Router(),
  List = require('./list.model');
 
-listRouter.get('/', (req, res) => {
-    res.send('Lists!');
+/**
+ * Get a single list by id
+ */
+listRouter.get('/:id', (req, res) => {
+    List.findById(req.params.id, (err, list) => {
+        if (!err) {
+            res.json(list);
+        } else {
+            handleError(err, res);
+        }
+    })
 });
+
+/**
+ * A generic error handler. Will log error and return status 500
+ */
+function handleError(err, res) {
+    console.log(`ERROR: ${err}`);
+    res.sendStatus(500);
+}
