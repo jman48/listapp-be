599aa3bee5598ec0e0206464361e0d7fa6cf4dd8
John Armstrong <j.armstrong484@gmail.com>
2016-11-20 13:01:35 +1300

    Added an endpoint to get all items of a list

--- item/item.controller.js
+++ item/item.controller.js
@@ -4,7 +4,23 @@ let express = require('express'),
     Item = require('./item.model').Model;
 
 itemRouter.get('/', (req, res) => {
-    res.send('Items!');
+    List.findById(res.params.listId, (err, list) => {
+        if (err) {
+            return handleError(err, res);
+        }
+
+        res.json(list.items);
+    });
 });
+
+/**
+ * A generic error handler. Will log error and return status 500
+ */
+function handleError(err, res) {
+    console.log(`ERROR: ${err}`);
+
+    //Do not send error message as we do not want to expose any internal working or our application
+    res.sendStatus(500);
+}
 
 module.exports = itemRouter;
