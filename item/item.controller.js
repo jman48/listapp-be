9c0a582b99c2c0c9ebf77edf8899d654381205e0
John Armstrong <j.armstrong484@gmail.com>
2016-11-20 13:07:16 +1300

    Added an endpoint to add a new item to a list

--- item/item.controller.js
+++ item/item.controller.js
@@ -3,6 +3,9 @@ let express = require('express'),
     List = require('../list/list.model'),
     Item = require('./item.model').Model;
 
+/**
+ * Get all items for a list
+ */
 itemRouter.get('/', (req, res) => {
     List.findById(res.params.listId, (err, list) => {
         if (err) {
@@ -14,12 +17,36 @@ itemRouter.get('/', (req, res) => {
 });
 
 /**
+ * Add a new item to a list
+ */
+itemRouter.post('/', (req, res) => {
+    List.findById(req.params.listId, (err, list) => {
+        if (err) {
+            return handleError(err, res);
+        }
+
+        let item = new Item();
+        item.name = req.body.name;
+        list.items.push(item);
+
+        list.save((err) => {
+            if (err) {
+                return handleError(err, res);
+            }
+
+            //Item created and added to list successfully
+            res.sendStatus(201);
+        });
+    });
+});
+
+/**
  * A generic error handler. Will log error and return status 500
  */
 function handleError(err, res) {
     console.log(`ERROR: ${err}`);
 
-    //Do not send error message as we do not want to expose any internal working or our application
+    //Do not send error message as we do not want to expose any internal working of our application
     res.sendStatus(500);
 }
 
